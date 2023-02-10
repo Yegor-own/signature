3b81dbb21648eb5d2195b8aed4e01e433c0caa85
Yego-own <egor891712@gmail.com>
2023-02-10 19:07:12 +0300

    feat: added c2c message redirection

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,21 +5,50 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 )
 
-func main() {
-	conn, _ := net.Dial("tcp", "localhost:8081")
+func readSock(conn net.Conn) {
 	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Type message: ")
-		msg, _ := reader.ReadString('\n')
-		fmt.Fprintf(conn, msg+"\n")
-
-		resp, err := bufio.NewReader(conn).ReadString('\n')
-		if err != nil {
-			fmt.Println(err.Error())
-			break
+		buf := make([]byte, 256)
+		readedLn, _ := conn.Read(buf)
+		if readedLn > 0 {
+			fmt.Println(string(buf))
 		}
-		fmt.Println("Response message:", resp)
 	}
+}
+
+func readConsole(ch chan string) {
+	for {
+		fmt.Println(">")
+		line, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		out := line[:len(line)-1]
+		ch <- out
+	}
+}
+
+func main() {
+	ch := make(chan string)
+	defer close(ch)
+
+	conn, _ := net.Dial("tcp", "localhost:8081")
+
+	go readConsole(ch)
+	go readSock(conn)
+
+	for {
+		val, ok := <-ch
+		if ok {
+			_, err := conn.Write([]byte(val))
+			if err != nil {
+				fmt.Println(err)
+				break
+			} else {
+				time.Sleep(2 * time.Second)
+			}
+		}
+	}
+
+	fmt.Println("Finished...")
+	conn.Close()
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,20 +1,50 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
+	"strings"
 )
 
-func process(conn net.Conn) {
+func process(conns map[int]net.Conn, n int) {
+	var clientNum int
+
+	conn := conns[n]
 	defer conn.Close()
 	for {
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Println("Message received:", string(message))
-		_, err := conn.Write([]byte("Lutoe " + message + "\n"))
+		buf := make([]byte, 256)
+		readedLn, err := conns[n].Read(buf)
 		if err != nil {
+			if err.Error() == "EOF" {
+
+				fmt.Println("Close", n)
+				delete(conns, n)
+			}
 			fmt.Println(err)
-			break
+		}
+
+		message := string(buf[:readedLn])
+
+		_, err = fmt.Sscanf(message, "%d", &clientNum)
+		if err != nil {
+			conn.Write([]byte(err.Error()))
+			continue
+		}
+
+		pos := strings.Index(message, " ")
+		if pos > 0 {
+			outMsg := message[pos+1:]
+			conn = conns[clientNum]
+			if conn == nil {
+				conns[n].Write([]byte("Connection is closed"))
+				continue
+			}
+			outBuf := []byte(fmt.Sprintf("%d >> %s\n", clientNum, outMsg))
+			_, err = conn.Write(outBuf)
+			if err != nil {
+				fmt.Println("Server: ", err)
+				break
+			}
 		}
 	}
 }
@@ -22,10 +52,14 @@ func process(conn net.Conn) {
 func main() {
 	fmt.Println("Server starting...")
 	ln, _ := net.Listen("tcp", ":8081")
+	conns := make(map[int]net.Conn, 1024)
+	i := 0
 
 	for {
 		conn, _ := ln.Accept()
-		go process(conn)
+		conns[i] = conn
+		go process(conns, i)
+		i++
 	}
 
 }
